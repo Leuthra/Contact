eb758ba1d291590a0a2815a7320cdb5a335d7d36
Leuthra <romidev20@gmail.com>
2024-03-19 23:59:17 +0700

    init

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,38 @@
-export default function Home() {
+import ContactTable from "@/components/contact-table";
+import Search from "@/components/search";
+import Pagination from "@/components/pagination";
+import { CreateButton } from "@/components/buttons";
+import { getContactPages } from "@/lib/data";
+import { Suspense } from "react";
+import { TableSkeleton } from "@/components/skeleton";
+
+const Contacts = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  const totalPages = await getContactPages(query);
+
   return (
-    <h1 className="title text-2xl">Home Page</h1>
+    <div className="max-w-screen-md mx-auto mt-5">
+      <div className="flex items-center justify-between gap-1 mb-5">
+        <Search />
+        <CreateButton />
+      </div>
+      <Suspense key={query + currentPage} fallback={<TableSkeleton />}>
+        <ContactTable query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="flex justify-center mt-4">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
   );
-}
+};
+
+export default Contacts;
